4fec2489f0477c5e57b906f6f028f5c72d41fc18
Mike Kelly <mkelly@hidoc.com.mx>
2017-02-13 22:50:18 -0600

    ENH: Admin and App
    Se agrego el administrador y la app configuration para el usuario.
    
    Miguel Kelly

--- HiDocBackEnd/email_user/apps.py
+++ HiDocBackEnd/email_user/apps.py
@@ -1,5 +1,9 @@
+"""App configuration for custom_user."""
 from django.apps import AppConfig
 
 
 class EmailUserConfig(AppConfig):
+    """Default configuration for custom_user."""
+
     name = 'email_user'
+    verbose_name = "User"
