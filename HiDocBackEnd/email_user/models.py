055c0ba1e88d391f3959bd659e391ad67d18bee4
Mike Kelly <mkelly@hidoc.com.mx>
2017-02-13 22:18:56 -0600

    EmailUser
    Se creo el usuario que el identificador es el email.
    
    Miguel Kelly

--- HiDocBackEnd/email_user/models.py
+++ HiDocBackEnd/email_user/models.py
@@ -7,11 +7,11 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
+
 class EmailUserManager(BaseUserManager):
+    """Custom manager for EmailUser."""
 
-    """ Custom manager for EmailUser."""
-
-    def _create_user(self, email, password, 
+    def _create_user(self, email, password,
                      is_staff, is_superuser, **extra_fields):
         """
         Create and save an EmailUser with the given email and password.
@@ -24,10 +24,9 @@ class EmailUserManager(BaseUserManager):
         :raise ValueError: email is not set
 
         """
-
         now = timezone.now()
         if not email:
-            rasie ValueError('The given email must be set')
+            raise ValueError('The given email must be set')
         email = self.normalize_email(email)
         is_active = extra_fields.pop("is_active", True)
         user = self.model(email=email, is_staff=is_staff, is_active=is_active,
@@ -46,7 +45,6 @@ class EmailUserManager(BaseUserManager):
         :return email_user.models.EmailUser user: regular user
 
         """
-
         is_staff = extra_fields.pop("is_staff", False)
         return self._create_user(email, password, is_staff, False,
                                  **extra_fields)
@@ -60,13 +58,11 @@ class EmailUserManager(BaseUserManager):
         :return email_user.models.EmailUser user: regular user
 
         """
-
         return self._create_user(email, password, True, True,
                                  **extra_fields)
 
 
 class AbstractEmailUser(AbstractBaseUser, PermissionsMixin):
-
     """
     Abstract user with the same behavior as Django's default user.
 
@@ -89,4 +85,52 @@ class AbstractEmailUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(
         _('staff status'), default=False, help_text=_(
             'Designates wether the user can log into the admin site.'))
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(_('active'), default=True, help_text=_(
+        'Designates whether this user should be treated as '
+        'active. Unselect this instead of deleating the acounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = EmailUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:  # noqa: D101
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        abstract = True
+
+    def get_full_name(self):
+        """Return the email."""
+        return self.email
+
+    def get_short_name(self):
+        """Return the email."""
+        return self.email
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this User."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+
+# Monkey patch Django 1.7 to avoid detecting migrations
+if django.VERSION[:2] == (1, 7):
+    last_login = AbstractEmailUser._meta.get_field('last_login')
+    last_login.blank = True
+    last_login.null = True
+    last_login.default = models.field.NOT_PROVIDED
+    groups = AbstractEmailUser._meta.get_field('groups')
+    groups.help_text = _('The groups this user belongs to. A user will get '
+                         'all permission granted to each of their groups.')
+
+
+class EmailUser(AbstractEmailUser):
+    """
+    Concrete class of AbstractEmailUser.
+
+    Use this if you don't need to extend EmailUser.
+
+    """
+
+    class Meta(AbstractEmailUser.Meta): # noqa:D101
+        swappable = 'AUTH_USER_MODEL'
